1fbedbd48089ca454d38adceef85782de64d4084
CrimeStein <yamori425@gmail.com>
2021-01-05 07:21:12 -0500

    Sentinel-Controlled Iteration

--- classavg.c
+++ classavg.c
@@ -7,24 +7,41 @@ int main( void )
   unsigned int counter; // number of grade to be entered next
   int grade; // grade value
   int total; // sum of grades entered by user
-  int average; // average of grades
+
+  float average; // number with decimal point for avg
 
   // intialize loop counter
   total = 0; // initialize total
-  counter = 1; // initialize loop counter
+  counter = 0; // initialize loop counter
 
   // processing phase
-  while ( counter <= 10 ) {
-  printf( "%s", "Enter grade: " ); // prompt for input
+  // get first grade from user
+  printf( "%s", "Enter grade, -1 to end: " ); // prompt input 
   scanf( "%d", &grade ); // read grade from user
-  total = total + grade; // add grade to total
-  counter = counter + 1; // increment counter
-} // end while
 
-  // termination phase
-  average = total / 10; // integer division
+  // loop while sentinel value not read from user
+  while ( grade != -1 ) {
+    total = total + grade; // add grade to total
+    counter = counter + 1; // increment counter
 
-  printf( "Class average is %d\n", average ); // display result
+  // get next grade from user
+    printf( "%s", "Enter grade, -1 to end: " ); // prompt for input
+    scanf( "%d", &grade ); // read next grade
+  } // end while
+
+  //termination phase
+  // if user entered at least one grade
+  if ( counter != 0 ) {
+  
+  // calculate average of all grades entered
+    average = ( float ) total / counter; // avoid truncation
+   
+   // display average with two digits of precision
+    printf( "Class average is %.2f\n", average );
+ } // end if 
+ else { // if no grades were input, output message
+  puts( "No grades were entered!" );
+  } // end else
 } // end function main
 
 
