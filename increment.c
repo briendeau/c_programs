e8081e31b5540348b280eba29ebf8e6be25c9999
CrimeStein <yamori425@gmail.com>
2021-01-09 07:24:47 -0500

    Increment & decrement operators

---
+++ increment.c
@@ -0,0 +1,27 @@
+// pre and post incrementing
+
+#include <stdio.h>
+
+int main( void )
+{
+  int c; // define var
+ 
+ // demonstrate post increment
+ c = 5; 
+ printf( "%d\n", c ); //print 5
+ printf( "%d\n", c++ ); // print 5 then postincrement
+ printf( "%d\n", c ) ; // print 6
+
+ //demonstrate preincrement
+ c = 5; //asign 5 to c
+ printf( "%d\n", c ); //print 5
+ printf( "%d\n", ++c ); // preincrement then print 6
+ printf( "%d\n", c ); //print 6
+} // end function main
+
+//note -- works similiarly
+
+
+
+
+ 
